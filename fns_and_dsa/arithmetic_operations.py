c4b57003da52ae17296db05361d5c42abbb69e09
Chukwu Chinonso Jeffrey <89136575+Nonijay@users.noreply.github.com>
2025-11-23 23:21:23 +0100

    Arithmetic operation module

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,17 @@
+# Creating an arithmetic operations 
+
+def addition (x, y):
+    return (x + y)
+
+def subtract (x, y):
+    return (x - y)
+
+def multiply (x, y):
+    return (x * y)
+
+def divide (x, y):
+    y == 0
+    print("Error: cannot be divided by 0")
+    return (x / y)
+
+print(f"Arithmetic operations success")
