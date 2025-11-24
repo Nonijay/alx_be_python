7f2e75fca6a11026c43523d2549661154595c22b
Chukwu Chinonso Jeffrey <89136575+Nonijay@users.noreply.github.com>
2025-11-24 13:36:27 +0100

    update params

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,6 +1,6 @@
 # Creating an arithmetic operations 
 
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
     op = operation.strip().lower()
 
     if op == 'add':
