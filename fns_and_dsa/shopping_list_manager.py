6773dccef293a9fce2c35129755f8443b05e118a
Chukwu Chinonso Jeffrey <89136575+Nonijay@users.noreply.github.com>
2025-11-24 21:34:45 +0100

    update variables

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,5 +1,5 @@
 def display_menu():
-    print (f"\n----Shopping List Manager----")
+    print (f"Shopping List Manager")
     print (f" ")
     print (f" 1. Add Item")
     print (f" 2. Remove Item")
